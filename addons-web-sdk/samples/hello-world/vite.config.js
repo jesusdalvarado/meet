0340bd660c9f7ccacdb252a6d96d286a0a3b9f49
Jesus <jesusd.alvarado@gmail.com>
2025-01-10 02:32:55 +0100

    Include src as part of build

--- addons-web-sdk/samples/hello-world/vite.config.js
+++ addons-web-sdk/samples/hello-world/vite.config.js
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite';
+import copy from 'rollup-plugin-copy';
 
 export default defineConfig({
   base: './', // Set the base path to relative
+
   build: {
     rollupOptions: {
+      plugins: [
+        copy({
+          targets: [
+            { src: 'src/**/*', dest: '../dist/hello-world' },
+          ],
+        }),
+      ],
       input: {
         main: './index.html' // Adjust to your project's HTML entry point
       },
